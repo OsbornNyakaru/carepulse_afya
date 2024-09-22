b557ad5b9cddadcaeb2e8197516f467c8945b100
osborn <osbornnyakaru44@gmail.com>
2024-09-22 10:51:02 +0300

    twilio and sentry integration

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -15,34 +15,40 @@ import { FormFieldType } from "./PatientForm"
 import { Doctors } from "@/constants"
 import { SelectItem } from "../ui/select"
 import Image from "next/image"
-import { createAppointment } from "@/lib/actions/appointment.actions"
+import { createAppointment, updateAppointment } from "@/lib/actions/appointment.actions"
+import { Appointment } from "@/types/appwrite.types"
 
 const AppointmentForm = ({
-    userId, patientId, type
+    userId, patientId, type, appointment, setOpen
 }: {
     userId: string;
     patientId: string;
     type: "create" | "cancel" | "schedule";
+    appointment?: Appointment;
+    setOpen: (open: boolean) => void;
 }) => {
     const router = useRouter();
     const [isLoading, setIsLoading] = useState(false);
 
     const AppointmentFormValidation = getAppointmentSchema(type);
   
+    console.log(appointment);
   // 1. Define your form.
   const form = useForm<z.infer<typeof AppointmentFormValidation>>({
     resolver: zodResolver(AppointmentFormValidation),
     defaultValues: {
-        primaryPhysician: "",
-        schedule: new Date(),
-        reason: "",
-        note: "",
-        cancellationReason: "",
+        primaryPhysician: appointment && appointment.primaryPhysician,
+        schedule: appointment ? new Date(appointment.schedule) : new Date(Date.now()),
+        reason: appointment ? appointment.reason : '',
+        note: appointment ? appointment.note : '',
+        cancellationReason: appointment?.cancellationReason ?? "",
     },
   });
   
   // 2. Define a submit handler.
   const onSubmit = async (values: z.infer<typeof AppointmentFormValidation>) => {
+    console.log('Im Submitting!', {type});
+    
     setIsLoading(true);
 
     let status;
@@ -75,8 +81,26 @@ const AppointmentForm = ({
             form.reset();
             router.push(`/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`)
         }
-      }
+      } else {
+        const appointmentToUpdate = {
+            userId, 
+            appointmentId: appointment?.$id!,
+            appointment: {
+                primaryPhysician: values?.primaryPhysician,
+                schedule: new Date(values?.schedule),
+                status: status as Status,
+                cancellationReason: values?.cancellationReason,
+            },
+            type
+        }
 
+        const updatedAppointment = await updateAppointment(appointmentToUpdate);
+
+        if (updatedAppointment) {
+            setOpen && setOpen(false);
+            form.reset();
+        }
+      }
     } catch (error) {
       console.error(error)
     } finally {
@@ -103,12 +127,14 @@ const AppointmentForm = ({
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-4">
-          <h1 className="header">New Appointment</h1>
-          <p className="text-dark-900">
-            Request a new appointment in 5 seconds 
-          </p>
-        </section>
+        {type === 'create' && 
+            <section className="mb-12 space-y-4">
+                <h1 className="header">New Appointment</h1>
+                    <p className="text-dark-900">
+                        Request a new appointment in 5 seconds 
+                    </p>
+            </section>
+        }       
 
         {type !== "cancel" && (
             <>
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7da03a4e3c5fd1892f6046f4209796b9@o4507989229436928.ingest.us.sentry.io/4507989243920384",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
